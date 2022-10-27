645d2c70b5237f6e0c052fc4ee3d2d796068f2c1
Ivan Nazarov <ivannz@yandex.ru>
2022-10-28 01:53:49 +0300

    a generator for the MIS problem

--- toybnb/milp.py
+++ toybnb/milp.py
@@ -1,4 +1,7 @@
 import numpy as np
+import scipy.sparse as sp
+import networkx as nx
+
 from numpy.random import default_rng
 from collections import namedtuple
 
@@ -6,7 +9,7 @@ MILP = namedtuple("MILP", "c,A_ub,b_ub,A_eq,b_eq,bounds,n,m")
 
 
 def generate(n: int, m: int, r: int, *, seed: int = None) -> MILP:
-    r"""A random instance of a generic MILP
+    r"""A random instance of a generic MILP of fixed size and structure
         $$
         \min\Bigl\{
             c^\top x
@@ -41,3 +44,45 @@ def generate(n: int, m: int, r: int, *, seed: int = None) -> MILP:
         # cost
         c = rng.normal(size=n)
         yield MILP(c, A_ub.T.copy(), b_ub, A_eq, b_eq, bounds, n, m)
+
+
+def incidence(G: nx.Graph) -> sp.coo_matrix:
+    """Get CSR sparse edge incidence matrix."""
+    eu, ev = map(np.array, zip(*G.edges))
+    e, one = np.arange(len(eu)), np.ones(len(eu))
+
+    E = sp.coo_matrix((one, (e, eu)), shape=(len(e), len(G)))
+    E += sp.coo_matrix((one, (e, ev)), shape=E.shape)
+
+    return E.tocsr()
+
+
+def generate_mis_ba(n: int = 400, m: int = 4, seed: int = None) -> MILP:
+    r"""A Maximum Independent Set problem in a Barabasi-Albert graph of fixed size.
+
+    For a undirected graph $G = (V, E)$ the Maximum Independent Set problem is
+        $$
+        \begin{aligned}
+            & \underset{x\in \{0, 1\}}{\text{maximize}}
+              & & \sum_{v \in G} x_v
+                  \\
+            & \text{subject to}
+              & & \forall uv \in E
+                  \colon x_u + x_v \leq 1
+                  \,.
+        \end{aligned}
+            $$
+    """
+    rng = default_rng(seed)
+    nilone = np.full((n, 2), (0, 1), float)
+    while True:
+        # get BA with n nodes and m preferentially attached edges
+        G = nx.barabasi_albert_graph(n, m, seed=rng)
+
+        c = np.full(len(G), -1.0)
+        A_ub = incidence(G)
+        b_ub = np.ones(len(G.edges))
+        A_eq = np.empty((0, n))
+        b_eq = np.empty((0,))
+
+        yield MILP(c, A_ub, b_ub, A_eq, b_eq, nilone, n, n)
