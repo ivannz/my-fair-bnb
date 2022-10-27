d0ac45945c1a21951661eab199eb21ea5685efec
Ivan Nazarov <ivannz@yandex.ru>
2022-10-28 01:33:46 +0300

    package skeleton

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    # update the version number
+    version = open("VERSION", "r").read().strip()
+
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(cwd, "toybnb", "__version__.py"), "w") as f:
+        f.write(f"__version__ = '{version}'\n")
+
+    setup(
+        name="toybnb",
+        version=version,
+        description="""A toy BnB solver for MILP""",
+        long_description=open("README.md", "rt").read(),
+        long_description_content_type="text/markdown",
+        license="MIT",
+        packages=[
+            "toybnb",
+        ],
+        python_requires=">=3.9",
+        install_requires=[
+            "numpy",
+            "networkx",
+            "scipy",
+        ],
+        test_requires=[
+            "gitpython",
+            "pytest",
+        ],
+    )
