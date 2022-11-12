f37f769672d8cf58f1cc8f7ed7b93beadbcf16af
Ivan Nazarov <ivannz@yandex.ru>
2022-11-12 20:36:20 +0300

    better CIP parser for model conversion

--- toybnb/scip/cip.py
+++ toybnb/scip/cip.py
@@ -2,97 +2,189 @@ import re
 import numpy as np
 from scipy import sparse as sp
 
+from collections import defaultdict
 from ..milp import MILP
 
 
 # regex for parsing pieces of CIP format
 # https://docs.python.org/3/library/re.html#simulating-scanf
-re_name, re_type = r"<([^>]+)>", r"\[([^\]]+)\]"
-re_float = r"([-+]?(?:\d+(?:\.\d*)?|\.\d+|)(?:[eE][-+]?\d+)?)"
-re_op, re_sign = r"([<>=]=)", r"([-+]?)"
+re_type = r"\[([^\]]*)\]"
+re_name = r"<([^>]*)>"
+re_float = r"[-+]?(?:\d+(?:\.\d*)?|\.\d+|)(?:[eE][-+]?\d+)?"
 
-pat_var = re.compile(
-    rf"^{re_type}\s*{re_name}\s*:\s*obj\s*=\s*{re_float}\s*,"
-    rf"\s*original\s*bounds\s*=\s*\[{re_float},\s*{re_float}\]$",
+pat_head = re.compile(rf"^\s*{re_type}\s*{re_name}\s*:\s*(.*)$", re.I)
+pat_var_obj = re.compile(rf"^obj=({re_float})\s*,?(.*)", re.I)
+pat_bounds = re.compile(
+    rf"^\s*(\S*)[^=]*=\s*\[+({re_float}),\s*({re_float})\]+,*(.*)$",
+    re.I,
+)
+pat_term = re.compile(
+    rf"^\s*(([-+]?)\s*({re_float})\s*(?:{re_name}(?:{re_type})?)?)\s*(.*)$",
+    re.I,
+)
+pat_rhs = re.compile(
+    rf"^\s*([<>=]=)\s*({re_float})\s*;\s*(.*)$",
     re.I,
 )
 
-pat_con_head = re.compile(rf"^{re_type}\s*{re_name}\s*:\s*(.*)$")
-pat_con_expr = re.compile(rf"^{re_sign}\s*{re_float}?\s*{re_name}\s*{re_type}\s*(.*)$")
-pat_con_last = re.compile(rf"^{re_op}\s*{re_float}\s*;$")
 
+def parse_expr(tail: str) -> tuple[dict, str]:
+    """Parse a linea expression with optional constaint term."""
+    # terms have a const by default
+    terms = defaultdict(float, [(None, 0.0)])
 
-def add_var(line: str, to: dict) -> str:
-    """Parse a variable declaration line and add it to the dict."""
-    m = pat_var.match(line)
-    if m is None:
-        return line
-
-    vt, name, coef, lb, ub = m.groups()
-
-    assert name not in to
-    to[name] = vt, (float(lb), float(ub)), float(coef)
-    return line
-
-
-def add_con(line: str, to: dict) -> str:
-    """Parse a constraint specification line and add it to the dict."""
-    tail = line
-
-    m = pat_con_head.match(line)
-    if m is None:
-        return line
-
-    ct, name, tail = m.groups()
-    assert ct.lower() == "linear"
-
-    terms = []
-    while tail:
-        m = pat_con_expr.match(tail)
-        if m is None:
+    m = pat_term.match(tail)
+    while m:
+        # standalone floats are constant terms
+        term, sign, val, var, _, tail = m.groups()
+        if not term:
             break
 
-        sign, val, var, vt, tail = m.groups()
-        coef = sign + ("1" if not val else val)
-        terms.append((var, float(coef)))
-    op, rhs = pat_con_last.match(tail).groups()
+        # standalone signs in front of variables have implicit one
+        if var is not None:
+            val = val if val else "1"
+        terms[var] += float(sign + val)
+        m = pat_term.match(tail)
 
-    assert name not in to
-    to[name] = terms, op, float(rhs)
-    return line
+    return terms, tail
 
 
-def parse_cip(cip: str) -> tuple[str, str, dict, dict]:
+def parse_var(tail: str) -> tuple[str, tuple, str]:
+    """Parse a variable declaration line."""
+    # get the type, variable name and its coefficient
+    type, var, tail = pat_head.match(tail).groups()
+    obj, tail = pat_var_obj.match(tail).groups()
+    if type not in ("integer", "binary", "continuous"):
+        raise NotImplementedError(type)
+
+    # fetch the bounds of the variable
+    lb, ub = float("-inf"), float("+inf")
+    m = pat_bounds.match(tail)
+    while m:
+        bound, b_lb, b_ub, tail = m.groups()
+        if bound not in ("original", "local", "global"):
+            raise NotImplementedError
+
+        lb = max(lb, float(b_lb))
+        ub = min(ub, float(b_ub))
+
+        m = pat_bounds.match(tail)
+
+    return var, (type, float(obj), lb, ub), tail
+
+
+def parse_con(tail: str) -> tuple[str, tuple, str]:
+    """Parse a constraint specification line."""
+    # get the type and name of the constraint
+    type, con, tail = pat_head.match(tail).groups()
+    if type not in ("linear"):
+        raise NotImplementedError(type)
+
+    # get the terms and then the right-hand side
+    terms, tail = parse_expr(tail)
+    assert tail
+
+    # subtract the rhs from the constant term
+    op, rhs, tail = pat_rhs.match(tail).groups()
+    terms[None] -= float(rhs)  # xxx substract rhs fomr lhs's const
+
+    # our constraints hanve implicit zero right-hand sides
+    return con, (type, op, dict(terms)), tail
+
+
+def parse_fix(tail: str) -> tuple[str, tuple, str]:
+    """Parse a fixed variable specification."""
+    # read the var part
+    name, var, tail = parse_var(tail)
+
+    # handle the transformation
+    type, sep, tail = tail.lstrip().partition(":")
+
+    # type can also be `negated`
+    if type not in ("fixed", "aggregated") or not sep:
+        raise NotImplementedError(type)
+
+    # parse the transformation
+    terms, tail = parse_expr(tail)
+    assert not tail
+
+    # fixed variables are both a constraint and a variable
+    terms[name] -= 1
+    return name, (var, ("linear", "==", dict(terms))), tail
+
+
+def parse_objective(sense: str, offset: float = 0.0, **ignore) -> tuple[str, float]:
+    """Extract the objective sense and the offset."""
+    assert sense in ("maximize", "minimize")
+    return sense[:3], float(offset)
+
+
+def cip_sections(filename: str) -> tuple[str, str]:
+    """Read CIP file line-by-line with section detection."""
+    sequence = {
+        "STATISTICS": {"OBJECTIVE"},
+        "OBJECTIVE": {"VARIABLES", "FIXED", "CONSTRAINTS", "END"},
+        "VARIABLES": {"FIXED", "CONSTRAINTS", "END"},
+        "FIXED": {"CONSTRAINTS", "END"},
+        "CONSTRAINTS": {"END"},
+    }
+
+    with open(filename, "rt") as f:
+        # get the name and create a new group
+        last, line = "", f.readline().strip()
+        assert line == "STATISTICS"
+        while line != "END":
+            group, sections = line, sequence[line]
+
+            # items in the group start with whitespace
+            last, line = line, f.readline().strip()
+            while line and line not in sections:
+                yield group, line
+                last, line = line, f.readline().strip()
+
+    return last
+
+
+def parse_cip(filename: str) -> tuple[str, str, dict, dict]:
     """A very simple non-resilient CIP file reader."""
-    with open(cip, "rt") as f:
-        assert f.readline().strip() == "STATISTICS"
-        _, name = re.split(r"\s*:\s*", f.readline().strip())
-        f.readline()
-        f.readline()
+    m_cons, m_vars, m_other = {}, {}, {}
+    for sec, line in cip_sections(filename):
+        if sec == "VARIABLES":
+            name, var, line = parse_var(line)
+            m_vars[name] = var
 
-        assert f.readline().strip() == "OBJECTIVE"
-        _, sense = re.split(r"\s*:\s*", f.readline().strip().lower())
-        assert sense in ("maximize", "minimize")
+        elif sec == "CONSTRAINTS":
+            name, con, line = parse_con(line)
+            m_cons[name] = con
 
-        assert f.readline().strip() == "VARIABLES"
-        m_vars = {}
-        while add_var(f.readline().strip(), m_vars) != "CONSTRAINTS":
-            pass
+        elif sec == "FIXED":
+            name, (var, con), line = parse_fix(line)
+            assert name not in m_vars
+            m_vars[name] = var
 
-        m_cons = {}
-        while add_con(f.readline().strip(), m_cons) != "END":
-            pass
-        assert not f.readline().strip()
+            assert name not in m_cons
+            m_cons["f_c_" + name] = con
 
-    return name, sense[:3], m_vars, m_cons
+        else:
+            head, tail = re.split(r"\s*:\s*", line)
+            m_other.setdefault(sec, {}).update([(head.lower(), tail)])
+            continue
+
+        assert not line
+
+    name = m_other["STATISTICS"].get("problem name")
+    objective = parse_objective(**m_other["OBJECTIVE"])
+
+    return name, objective, m_vars, m_cons
 
 
 def from_cip(filename: str) -> MILP:
-    name, sense, m_vars, m_cons = parse_cip(filename)
+    # we don't care about the offset
+    name, (sense, _), m_vars, m_cons = parse_cip(filename)
 
-    # split the vars in two groups
+    # split the vars in two groups: integer and continuous
     v_int, v_con = [], []
-    for v, (vt, _, _) in m_vars.items():
+    for v, (vt, *_) in m_vars.items():
         which = v_int if vt in ("binary", "integer") else v_con
         which.append(v)
 
@@ -100,38 +192,45 @@ def from_cip(filename: str) -> MILP:
     lut = {v: j for j, v in enumerate(v_int + v_con)}
     m, n = len(v_int), len(v_int) + len(v_con)
 
-    # split the constraints into eq and ineq
-    m_cons_eq, m_cons_ineq = [], []
-    for lhs, op, rhs in m_cons.values():
+    # normalize the constraints and split them into eq and ub
+    m_cons_eq, m_cons_ub = [], []
+    for _, op, terms in m_cons.values():
         assert op in ("<=", "==", ">=")
-        which = m_cons_eq if op == "==" else m_cons_ineq
-        which.append((lhs, op, rhs))
+        # the parser returns constraint specs with implicit rhs zero
+        #  and explicit lhs const term, get it and convert into
+        #  `lhs | op | rhs` form. Also flip constraints with `>=` op
+        sign = +1.0
+        if op == ">=":
+            sign, op = -1.0, "<="
+
+        lhs = {k: sign * v for k, v in terms.items() if k is not None}
+        rhs = -terms[None] * sign
+
+        which = m_cons_eq if op == "==" else m_cons_ub
+        which.append((lhs, rhs))
 
     # rebuild A_ub x <= b_ub
-    b_ub = np.full(len(m_cons_ineq), np.nan, float)
-    A_ub = sp.lil_array((len(m_cons_ineq), n), dtype=float)
-    for j, (lhs, op, rhs) in enumerate(m_cons_ineq):
-        # flip the sign of geq consraints
-        s = +1 if op == "<=" else -1
-
-        b_ub[j] = rhs * s
-        for v, coef in lhs:
-            A_ub[j, lut[v]] = coef * s
+    b_ub = np.full(len(m_cons_ub), np.nan, float)
+    A_ub = sp.lil_array((len(m_cons_ub), n), dtype=float)
+    for j, (lhs, rhs) in enumerate(m_cons_ub):
+        b_ub[j] = rhs
+        for v, coef in lhs.items():
+            A_ub[j, lut[v]] = coef
 
     # rebuild A_eq x == b_eq
     b_eq = np.full(len(m_cons_eq), np.nan, float)
     A_eq = sp.lil_array((len(m_cons_eq), n), dtype=float)
-    for j, (lhs, _, rhs) in enumerate(m_cons_eq):
+    for j, (lhs, rhs) in enumerate(m_cons_eq):
         b_eq[j] = rhs
-        for v, coef in lhs:
+        for v, coef in lhs.items():
             A_eq[j, lut[v]] = coef
 
     # reconstruct the cost vector and the bounds
     c = np.zeros(len(lut))
     bounds = np.full((len(lut), 2), (-np.inf, +np.inf))
-    for v, (_, lu, coef) in m_vars.items():
-        c[lut[v]] = coef
-        bounds[lut[v]] = lu
+    for v, (_, obj, lb, ub) in m_vars.items():
+        c[lut[v]] = obj
+        bounds[lut[v]] = lb, ub
 
     # flip the cost vector if the problem is maximization
     if sense == "max":
